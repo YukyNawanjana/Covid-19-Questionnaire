6b5ff8ec3f65bfd53ab16b1485a44178c8c2f38c
YukyNawanjana <yasirunawanjana70@gmail.com>
2020-10-09 11:02:01 +0530

    question display

--- app.js
+++ app.js
@@ -1,42 +1,50 @@
 const quizData = [
     {
-        question: 'How old is floring?',
-        a: '10',
-        b: '17',
-        c: '26',
-        d: '110',
-        correct: 'c'
-    },
-    {
-        question: 'what is the best programing language in 2020',
-        a: 'Java',
-        b: 'python',
-        c: 'PHP',
-        d: 'JavaScript',
-        correct: 'd'
-    },
-    {
-        question: 'How is the president of US?',
-        a: 'Floring pop',
-        b: 'Donal trump',
-        c: 'Barak Obama',
-        d: 'Mihai Andri',
+        question: 'Do you have a fever?',
+        a: 'Yes',
+        b: 'No',
         correct: 'b'
     },
     {
-        question: 'what does HTML stand for?',
-        a: 'Hypertext Markup Language',
-        b: 'Cascading style sheet',
-        c: 'Jason object Notation',
-        d: 'Aplication Programing Interface',
+        question: 'Are you close contact with sick people?',
+        a: 'Yes',
+        b: 'No',
+        correct: 'b'
+    },
+    {
+        question: 'Do you wash your hands often?',
+        a: 'Yes',
+        b: 'No',
         correct: 'a'
     },
     {
-        question: 'What year was JavaScript Launched??',
-        a: '1996',
-        b: '1995',
-        c: '1994',
-        d: '1993',
+        question: 'Do you using a face mask?',
+        a: 'Yes',
+        b: 'No',
+        correct: 'a'
+    },
+    {
+        question: 'Do you have any immune disease?',
+        a: 'Yes',
+        b: 'No',
+        correct: 'b'
+    },
+    {
+        question: 'Do you ofthen touch your face?',
+        a: 'Yes',
+        b: 'No',
+        correct: 'b'
+    },
+    {
+        question: 'Are you coughing?',
+        a: 'Yes',
+        b: 'No',
+        correct: 'b'
+    },
+    {
+        question: 'Are you travel during lockdown period?',
+        a: 'Yes',
+        b: 'No',
         correct: 'b'
     },
 
@@ -51,8 +59,6 @@ const question = document.getElementById('qestion');
 const quiz = document.getElementById('quiz');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 
 
@@ -63,6 +69,7 @@ startBtn.addEventListener('click', ()=>{
     if(name.value){
         cardBody.classList.add('d-none');
         cardQuiz.classList.remove('d-none');
+        loadQuiz();
     }else{
         alert.classList.remove('d-none');
         alert.innerHTML = `<strong>Please Enter your Name</strong>`;
@@ -74,3 +81,51 @@ startBtn.addEventListener('click', ()=>{
 })
 
 
+function loadQuiz(){
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+
+    question.innerHTML = currentQuizData.question;
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b; 
+}
+
+function deselectAnswers(){
+    answerEls.forEach((answerEl) =>{
+        answerEl.checked = false;
+    });
+
+}
+
+function getSelected(){
+   
+
+    let answer = undefined
+    answerEls.forEach((answerEl) =>{
+        if(answerEl.checked){
+            answer =  answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+submitBtn.addEventListener('click', ()=>{
+    const answer = getSelected();
+
+    console.log(answer);
+    if(answer){
+        if(answer === quizData[currentQuiz].correct){
+            score++;
+        }
+        currentQuiz++;
+        
+        if(currentQuiz < quizData.length){
+            loadQuiz();
+        }else{
+            // TODO Show Results
+            //quiz.innerHTML = `<h2>Your anser corectly ${score}/${quizData.length} questions</h2><button onClick="location.reload()">Reload</button>`;
+        }
+    }    
+    
+});
