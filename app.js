ee1673476359c07c4922850d958a92b1f27ec192
YukyNawanjana <yasirunawanjana70@gmail.com>
2020-10-09 12:37:18 +0530

    bug fixed

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const quizData = [
         a: 'Yes',
         b: 'No',
         correct: 'b'
-    },
+    }
 
 ];
 const startBtn = document.getElementById('startBtn');
@@ -56,7 +56,6 @@ const name = document.getElementById('name');
 const alert = document.querySelector('.alert');
 const answerEls = document.querySelectorAll('.answer');
 const question = document.getElementById('qestion');
-const quiz = document.getElementById('quiz');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const submitBtn = document.getElementById('submit');
@@ -92,29 +91,32 @@ function loadQuiz(){
     
 }
 
-function deselectAnswers(){
-    answerEls.forEach((answerEl) =>{
-        answerEl.checked = false;
-    });
-}
-
 
 function getSelected(){
-    let answer = undefined;
+   
+
+    let answer = undefined
     answerEls.forEach((answerEl) =>{
         if(answerEl.checked){
             answer =  answerEl.id;
         }
     });
+
     return answer;
 }
 
 
+function deselectAnswers(){
+    answerEls.forEach((answerEl) =>{
+        answerEl.checked = false;
+    });
+
+}
 
 submitBtn.addEventListener('click', ()=>{
-    
     const answer = getSelected();
-    //console.log(answer);
+
+    console.log(answer);
     if(answer){
         if(answer === quizData[currentQuiz].correct){
             score++;
@@ -125,15 +127,8 @@ submitBtn.addEventListener('click', ()=>{
             loadQuiz();
         }else{
             // TODO Show Results
-            //quiz.innerHTML = `<h2>Your anser corectly ${score}/${quizData.length} questions</h2><button onClick="location.reload()">Reload</button>`;
+            cardQuiz.innerHTML = `<h2>Your anser corectly ${score}/${quizData.length} questions</h2><button onClick="location.reload()">Reload</button>`;
         }
-    }else{
-        alert.classList.remove('d-none');
-        alert.innerHTML = `<strong>Please Select Answer</strong>`;
-
-        setTimeout(()=>{
-            alert.classList.add('d-none');
-        }, 2000);
-    }   
+    }    
     
 });
