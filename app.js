3d3f1c90f76283f94a1171ea26f907c8e94b18c9
YukyNawanjana <yasirunawanjana70@gmail.com>
2020-10-17 19:45:10 +0530

    animation added

--- app.js
+++ app.js
@@ -226,3 +226,34 @@ submitBtn.addEventListener('click', ()=>{
 
 
 
+// Animations
+
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left',{
+    origin : 'left',
+    duration: 1000,
+    distance:'25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right',{
+    origin : 'right',
+    duration: 1000,
+    distance:'25rem',
+    delay: 600
+});
+
+
+sr.reveal('.animate-top',{
+    origin : 'top',
+    duration: 1000,
+    distance:'25rem',
+    delay: 600
+});
+
+
+sr.reveal('.animate-bottom',{
+    delay: 600
+});
